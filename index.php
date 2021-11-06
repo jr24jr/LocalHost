3bcd85b066c93e145ee1da3ba0f47279114b3ea7
jr24jr <bravo2466@gmail.com>
2021-11-06 01:48:53 -0500

    orden de lista y carpeta svg

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link rel="icon" href="https://www.flaticon.es/premium-icon/icons/svg/3115/3115458.svg">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container-fluid">
@@ -14,56 +14,67 @@
 <div class="row">
 <?php
 $dato=array();
+$carpeta=array();
+$files=array();
 $pos=0;
+$j=0;
 $directorio = opendir(".");
 while ($archivo = readdir($directorio)){
 	if($archivo!="." && $archivo!=".."){
 		$separa=explode(".",$archivo);
 		if(count($separa)==1){
-			$dato[$pos]=[
+			$carpeta[$j]=[
 				'tipo'=>'carpeta',
 				'ruta'=>$archivo
 			];
 		}
 		else{
 			if(count($separa)==2){
-				$dato[$pos]=[
+				$files[$pos]=[
 					'tipo'=>$separa[1],
 					'ruta'=>$archivo
 				];
 			}
 			else{
-				$dato[$pos]=[
+				$files[$pos]=[
 					'tipo'=>$separa[count($separa)-1],
 					'ruta'=>$archivo
 				];
 			}
 		}
+		$j++;
 		$pos++;
 	}
 }
-foreach ($dato as  $valor) {
-	if($valor['tipo']=='carpeta'){
+foreach ($carpeta as  $valor) {
+	echo "<div class='col-4 mt-3 mb-3'><div class='card bg-transparent'>";
+	echo '<div class="align-self-center">
+		<a href="/'.$valor['ruta'].'">
+			<img class="card-img-top" src="svg/folder.svg" style="width: 10rem;">
+		</a>
+	</div><br>';
+	echo "<span class='text-center text-white'><a href='/".$valor['ruta']."'>".$valor['ruta']."</a></span>";
+	echo "</div></div>";
+} 
+foreach ($files as  $valor) {
+	if($valor['ruta']!='index.php'){
 		echo "<div class='col-4 mt-3 mb-3'><div class='card bg-transparent'>";
-		echo '<div class="align-self-center">
-			<a href="/'.$valor['ruta'].'">
-				<img class="card-img-top" src="svg/folder.svg" style="width: 10rem;">
-			</a>
-		</div><br>';
-		echo "<span class='text-center text-white'><a href='/".$valor['ruta']."'>".$valor['ruta']."</a></span>";
-		echo "</div></div>";
-	}
-	else{
-		if($valor['ruta']!='index.php'){
-			echo "<div class='col-4 mt-3 mb-3'><div class='card bg-transparent'>";
+		if($valor['tipo']=="png" || $valor['tipo']=="jpg" || $valor['tipo']=="svg" || $valor['tipo']=="ico"){
+			echo '<div class="align-self-center">
+				<a href="/'.$valor['ruta'].'">
+					<img class="card-img-top" src="'.$valor['ruta'].'" style="width: 10rem;">
+				</a>
+			</div><br>';
+		}
+		else{
 			echo '<div class="align-self-center">
 				<a href="/'.$valor['ruta'].'">
 					<img class="card-img-top" src="svg/'.$valor['tipo'].'.svg" style="width: 10rem;">
 				</a>
 			</div><br>';
-			echo "<span class='text-center text-white'><a href='/".$valor['ruta']."'>".$valor['ruta']."</a></span>";
-			echo "</div></div>";
 		}
+		echo "<span class='text-center text-white'><a href='/".$valor['ruta']."'>".$valor['ruta']."</a></span>";
+		echo "</div></div>";
 	}
 } 
 ?>
